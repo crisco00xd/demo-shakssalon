f1829a5fb47f8f7b35b5cf128f84cb665b2c4a2f
Cristian J. Rivera Santiago <cristian@scoreinc.com>
2024-01-30 22:51:23 -0400

    [FEAT] Added logo and other stuff

--- components/Header/SocialIconsLight.js
+++ components/Header/SocialIconsLight.js
@@ -1,27 +1,29 @@
 // import { InstagramLogo, FacebookLogo, Moon } from 'phosphor-react';
-import { LogoInstagram, LogoFacebook, LogoYoutube } from '@carbon/icons-react';
-import Link from 'next/link';
+import { LogoInstagram, LogoFacebook, LogoYoutube } from "@carbon/icons-react";
+import Link from "next/link";
 
 export default function SocialIconsLight() {
-	return (
-		<ul className='social-icons' role='list'>
-			<li className='social-icons__item'>
-				<Link
-					href='https://instagram.com/'
-					target='_blank'
-					className='social-icons__link'>
-					<LogoInstagram size={30} />
-				</Link>
-			</li>
-			<li className='social-icons__item'>
-				<Link
-					href='https://www.facebook.com/'
-					target='_blank'
-					className='social-icons__link'>
-					{/* <FacebookLogo size={32} color='black' weight='fill' /> */}
-					<LogoFacebook size={30} />
-				</Link>
-			</li>
-		</ul>
-	);
+  return (
+    <ul className="social-icons" role="list">
+      <li className="social-icons__item">
+        <Link
+          href="https://www.instagram.com/shakssalon_/"
+          target="_blank"
+          className="social-icons__link"
+        >
+          <LogoInstagram size={30} />
+        </Link>
+      </li>
+      <li className="social-icons__item">
+        <Link
+          href="https://www.facebook.com/profile.php?id=100085374409111"
+          target="_blank"
+          className="social-icons__link"
+        >
+          {/* <FacebookLogo size={32} color='black' weight='fill' /> */}
+          <LogoFacebook size={30} />
+        </Link>
+      </li>
+    </ul>
+  );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,107 +1,109 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { createClient } from 'contentful';
-import { EnvelopeSimple, Phone, MapPin } from 'phosphor-react';
+import Head from "next/head";
+import Link from "next/link";
+import { createClient } from "contentful";
+import { EnvelopeSimple, Phone, MapPin } from "phosphor-react";
 
 export default function Contact({}) {
-	const title = 'Contact Us';
+  const title = "Contact Us";
 
-	async function handleOnSubmit(e) {
-		e.preventDefault();
+  async function handleOnSubmit(e) {
+    e.preventDefault();
 
-		const formData = {};
+    const formData = {};
 
-		Array.from(e.currentTarget.elements).forEach((field) => {
-			if (!field.name) return;
-			formData[field.name] = field.value;
-		});
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
 
-		await fetch('/api/mail', {
-			method: 'POST',
-			body: JSON.stringify(formData),
-		});
+    await fetch("/api/mail", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
 
-		e.target.reset();
-	}
+    e.target.reset();
+  }
 
-	return (
-		<>
-			<Head>
-				<title>Next Salon | {title}</title>
-			</Head>
-			<div className='page-header'>
-				<h1 className='page-title'>{title}</h1>
-				<ul className='breadcrumbs' role='list'>
-					<li className='breadcrumbs__item'>
-						<Link href='/' className='breadcrumbs__link'>
-							Home
-						</Link>
-					</li>
-					<li className='breadcrumbs__item'>{title}</li>
-				</ul>
-			</div>
+  return (
+    <>
+      <Head>
+        <title>Shaks Salon | {title}</title>
+      </Head>
+      <div className="page-header">
+        <h1 className="page-title">{title}</h1>
+        <ul className="breadcrumbs" role="list">
+          <li className="breadcrumbs__item">
+            <Link href="/" className="breadcrumbs__link">
+              Home
+            </Link>
+          </li>
+          <li className="breadcrumbs__item">{title}</li>
+        </ul>
+      </div>
 
-			<div className='contact-us grid'>
-				<div className='wrapper wrapper--contact-us'>
-					<div className='contact-us__details'>
-						<h2 className='contact-us__title'>Get In Touch</h2>
-						<p className='contact-us__description'>
-							If you have any questions or require further information on any of
-							our treatments or services, feel free to get in touch.
-						</p>
-						<ul className='contact-us__details-list'>
-							<li className='contact-us__list-item'>
-								<EnvelopeSimple size={24} weight='light' color='#000' />
-								<a href='mailto: jake@weblavida.com'>hello@nextsalon.com</a>
-							</li>
-							<li className='contact-us__list-item'>
-								<Phone size={24} weight='light' color='#000000' />
-								(001) 12312 3452
-							</li>
-							<li className='contact-us__list-item'>
-								<MapPin size={24} weight='light' color='#000' />
-								123 Main St, Suite 200, Anytown, UK 00000
-							</li>
-						</ul>
-					</div>
-					<form
-						className='contact-us__form'
-						method='post'
-						onSubmit={handleOnSubmit}>
-						<div className='contact-us__form-group'>
-							<p className='contact-us__form-field'>
-								<label htmlFor='name'>Name</label>
-								<input type='text' name='name' required />
-							</p>
-							<p className='contact-us__form-field'>
-								<label htmlFor='email'>Email</label>
-								<input type='text' name='email' required />
-							</p>
-						</div>
+      <div className="contact-us grid">
+        <div className="wrapper wrapper--contact-us">
+          <div className="contact-us__details">
+            <h2 className="contact-us__title">Contactanos!</h2>
+            <p className="contact-us__description">
+              Si tiene alguna pregunta o necesita más información sobre
+              cualquiera de nuestros tratamientos o servicios, no dude en
+              ponerse en contacto.
+            </p>
+            <ul className="contact-us__details-list">
+              <li className="contact-us__list-item">
+                <EnvelopeSimple size={24} weight="light" color="#000" />
+                <a href="mailto: jake@weblavida.com">shakssalon@gmail.com</a>
+              </li>
+              <li className="contact-us__list-item">
+                <Phone size={24} weight="light" color="#000000" />
+                (787)689-6405
+              </li>
+              <li className="contact-us__list-item">
+                <MapPin size={24} weight="light" color="#000" />
+                37 Calle Francisco M Quinonez, Sabana Grande, 00637
+              </li>
+            </ul>
+          </div>
+          <form
+            className="contact-us__form"
+            method="post"
+            onSubmit={handleOnSubmit}
+          >
+            <div className="contact-us__form-group">
+              <p className="contact-us__form-field">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" required />
+              </p>
+              <p className="contact-us__form-field">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" required />
+              </p>
+            </div>
 
-						<div className='contact-us__form-group'>
-							<p className='contact-us__form-field'>
-								<label htmlFor='phone'>Phone</label>
-								<input type='text' name='phone' />
-							</p>
-							<p className='contact-us__form-field'>
-								<label htmlFor='subject'>Subject</label>
-								<input type='text' name='subject' required />
-							</p>
-						</div>
+            <div className="contact-us__form-group">
+              <p className="contact-us__form-field">
+                <label htmlFor="phone">Phone</label>
+                <input type="text" name="phone" />
+              </p>
+              <p className="contact-us__form-field">
+                <label htmlFor="subject">Subject</label>
+                <input type="text" name="subject" required />
+              </p>
+            </div>
 
-						<div className='contact-us__form-group'>
-							<p className='contact-us__form-field'>
-								<label htmlFor='message'>Message</label>
-								<textarea name='message' required></textarea>
-							</p>
-						</div>
-						<button className='contact-us__btn btn' type='submit'>
-							Send Message
-						</button>
-					</form>
-				</div>
-			</div>
-		</>
-	);
+            <div className="contact-us__form-group">
+              <p className="contact-us__form-field">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" required></textarea>
+              </p>
+            </div>
+            <button className="contact-us__btn btn" type="submit">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
